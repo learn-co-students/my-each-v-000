c11fdb4275767ef1d0a1441954bff8940619800f
Shana L Moore <shana.lavina.moore@gmail.com>
2015-11-12 16:10:22 -0800

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,11 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(words)
+  i = 0
+  while i < words.length
+    yield words[i]
+      i += 1
+  end
+  words
 end
+
+collection = [1, 2, 3, 4]
+my_each(collection) { |i| puts i }
