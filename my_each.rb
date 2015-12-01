d2bfd03ea032a720eac4c43105f98a73bed277c7
Karuna <imkaruna@gmail.com>
2015-12-01 02:56:10 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
+def my_each(words) # put argument(s) here
   # code here
+  i=0
+  length = words.length
+  until i == length
+yield words[i]
+    i+=1
+  end
+return words
 end
