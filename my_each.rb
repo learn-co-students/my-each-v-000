ae7d60309bbac0598833b67288a54ccd89c56d06
Lukeghenco <lukeghenco@gmail.com>
2015-12-13 14:10:03 -0500

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,14 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  if block_given?
+    array.length.times {|i| yield(array[i])}
+    array
+  else 
+    "No block given"
+  end
 end
+
+
+# call it
+collection = [1,2,3,4]
+
+my_each(collection) {|i| puts i}
