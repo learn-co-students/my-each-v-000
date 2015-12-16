6b275213e5a777fa4d9a666433ed55e4952346b2
jamcelr <jamcelr@hotmail.com>
2015-12-16 12:50:50 -0500

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+collection = [1,2,3,4]
+
+def my_each(array)
+  array.collect do |e|
+    yield(e)
+  end
+  array
+end
+
+my_each(collection) do |i|
+  puts i
 end
