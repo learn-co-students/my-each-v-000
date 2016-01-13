9e5e4dea766052f7b6f022f7e3fedc297cac9cc4
jenjiyi <jenny.yamada@gmail.com>
2016-01-13 09:55:34 -0800

    done with lab

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,17 @@
-def my_each # put argument(s) here
+def my_each(array) # put argument(s) here
   # code here
+  if block_given?
+    i=0
+    while i < array.size
+      yield (array[i])
+        i += 1
+      end
+     array
+  else
+     "no block given"
+  end
+end
+collection = [1, 2, 3, 4]
+my_each(collection) do |i| 
+   i 
 end
