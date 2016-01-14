f5e9a05c29721e21880abf03c0c8964ef257d1b0
benser1 <briganenser28@gmail.com>
2016-01-14 13:50:52 -0700

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,18 @@
-def my_each # put argument(s) here
-  # code here
+
+
+def my_each(array)
+
+  i = 0 
+  while i < array.length
+    yield(array[i])
+    i += 1
+  end
+  array
+  
+end
+
+my_each("string") do |num|
+  if num.length < 100
+     "We will keep counting to #{num}"
+  end
 end
