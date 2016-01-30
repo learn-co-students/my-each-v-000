d1274fefacc11522c3bd7305e47762eddbcc15a7
abadfish <kirsten.ofarrell@gmail.com>
2016-01-30 07:07:36 -0500

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array) # put argument(s) here
+  i = 0
+  while i < array.length 
+    yield(array[i])
+    i += 1# code here
+  end
+  array
+end
+
+my_each([6,7,8]) do |x|
+  puts "This block should not run!"
 end
