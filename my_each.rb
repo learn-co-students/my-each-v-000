127fcae0c3ed1a0ce05d5fcd4d1219f40dfa1c11
CodeByLine <yleventhal@ymail.com>
2016-02-17 02:07:57 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
+def my_each(lunch) # put argument(s) here
   # code here
+  lngth = 0
+  while lngth < lunch.size
+    yield lunch[lngth]   #"#{lunch}"
+    lngth += 1
+
+  end
+lunch
 end
+
+my_each(["ham","cheese","veggie"]) {puts "Let's eat!"}
+
