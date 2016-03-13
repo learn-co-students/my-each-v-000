58e51751fef1f26e33051dca608c9172170434e6
lamass <hadzy@yahoo.com>
2016-03-13 18:37:33 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,16 @@
+collection = [ 1,"jay", "marey","Samoa", 33, {mykey: "myvalue"}]
+
+
 def my_each(collection)
-  countercounter = 0
+  counter = 0
   while counter < collection.size
      yield collection[counter]
      counter += 1
   end
   collection
+end
+
+
+my_each(collection) do |x|
+   x
 end
