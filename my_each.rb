e416f219784916e7d3f2900d74fe812d939431ee
PeteAJ <peter.jacobson@gmail.com>
2016-03-14 15:28:10 -0400

    Done.

--- my_each.rb
+++ my_each.rb
@@ -13,20 +13,3 @@ end
 
 
 
-#def my_each(collection)
-
- # i = 0
-  #while i < collection.length
-   # yield(collection[i])
-    #i = i + 1
-  #end
-  #collection
-#end
- 
-#puts collection
-
-#my_each([collection[i]) { |name| puts "Hi, #{name}" }
-#collection = [1, 2, 3, 4]
-#my_each(collection) do |i|
-#  puts i
-#end
