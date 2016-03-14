383bf34d91112f256413c0c7cc7870fb094d8c27
PeteAJ <peter.jacobson@gmail.com>
2016-03-14 15:23:14 -0400

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,18 +1,31 @@
-def my_each(array) 
-   if block_given?
- i = 0
-  while i < array.length
-    yield array[i]
-    i = i + 1
-  end
+
+def my_each(array)
+ if block_given?
+   i = 0
+   while i < array.length
+     yield(array[i])
+     i = i + 1
+   end
   array
-else
-  puts array
 end
 end
 
 
 
+
+#def my_each(collection)
+
+ # i = 0
+  #while i < collection.length
+   # yield(collection[i])
+    #i = i + 1
+  #end
+  #collection
+#end
+ 
+#puts collection
+
+#my_each([collection[i]) { |name| puts "Hi, #{name}" }
 #collection = [1, 2, 3, 4]
 #my_each(collection) do |i|
 #  puts i
