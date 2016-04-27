1e5a4a73b689e941fc72bd2a567249c6fbced925
Llubomir Stanojlovic <candid.llama@gmail.com>
2016-04-27 17:27:43 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
+def my_each (var)# put argument(s) here
   # code here
+  index=0
+  while index < var.length
+    yield(var[index])
+    index += 1
+  end
+  var
 end
