e2773e4528c3a8033f99285698645ad76ee1a349
Nicholas-Maxwell <nick.maxwell18@gmail.com>
2016-05-03 19:23:42 -0400

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,26 @@
-def my_each # put argument(s) here
-  # code here
-end
+def my_each(array)
+
+i = 0
+
+while i < array.length
+    yield array[i] #---> yeilds the correct element
+    i+=1
+    end 
+  array #---> returned array containts the same element as the original
+end  
+
+#does not call on puts
+#iterates over each element
+
+
+
+ #can handle an empty collection
+  #returned array contains the same elements as the original collection
+  #does not modify the original collection
+  #block is run n times
+  #only single element is passed into block
+
+
+
+
+
