095395a6fa12342ac4aadeb07378c4b44d3346d3
bjacooper <bjacooper@gmail.com>
2016-05-04 23:42:32 +0100

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(watermelon)
+  i = 0
+  while i < watermelon.length 
+    yield(watermelon[i])
+    i = i + 1
+  end
+  watermelon
+end
+
+my_each(['hi', 'hello', 'bye', 'goodbye']) do |name|
+  "#{name}"
 end
