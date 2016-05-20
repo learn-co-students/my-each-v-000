fefa0028e2a756f66a5856a67d110a2c02a025a3
Dom McKellar <dom.mckellar@gmail.com>
2016-05-20 10:45:02 -0700

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
-  # code here
-end
+words = [1, 2, 3, 4]
+def my_each(words)
+  count = 0
+  while count < words.length
+    yield words[count]
+    count += 1
+  end
+   words
+ end
+#
