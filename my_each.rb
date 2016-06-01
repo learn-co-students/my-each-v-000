08924f1a80e0fee0a7cda985653c71270eab3f72
Michael Stewart <upp_mike@yahoo.com>
2016-05-31 19:37:04 -0700

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,22 @@
-def my_each # put argument(s) here
+def my_each(array)# put argument(s) here
   # code here
+  counter = 0
+  while counter < array.length
+    yield(array[counter])
+    counter += 1
+  end
+  array
 end
+
+# Here's my function call, commented out because it will make
+# the rspec test fail, because it doesn't want to see the word
+# "puts" anywhere in the file. All we were supposed to do is
+# write the function above. But if you want to run the program,
+# uncomment the below, and make sure to pull the # sign off the
+# front of puts. Put that there so rspec won't recognize "puts"
+=begin
+my_each([1,2,3,4]) {
+  |i|
+  #puts i
+}
+=end
