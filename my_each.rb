99db6e62fb4053c323cbf579c2ea15fbb2df4ae0
bging4 <wgingher@gmail.com>
2016-06-13 14:51:36 -0400

    Complete

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  unless array.empty?
+    counter = 0
+    while counter < array.length
+      yield(array[counter])
+      counter += 1
+    end
+  else
+    "This block should not run!"
+  end
+  array
 end
