3957e011c3840ed1ea03e9a4f15cd52b24c926da
John Verdone <jmverdone@gmail.com>
2016-08-07 21:54:38 -0400

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,11 @@
-def my_each # put argument(s) here
+def my_each(input)# put argument(s) here
   # code here
+    i = 0
+    while i < input.length 
+        yield(input[i])
+        i += 1
+    end
+    input
 end
+
+
