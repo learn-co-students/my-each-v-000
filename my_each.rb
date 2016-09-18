a92e7720156c23fcd7839a9e7cc03a792a5b8b84
Elvin <elvin@elvingarcia.com>
2016-09-18 17:10:39 -0400

    Done

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,17 @@
-def my_each # put argument(s) here
-  # code here
+
+
+
+def my_each (array) 
+  if block_given?
+    index=0
+      while index < array.length
+        yield(array[index])
+        index+=1
+      end
+  end
+  array
 end
+
+tas = ['arel', 'jon', 'logan', 'spencer']
+ my_each(tas){|obj| obj}
+
