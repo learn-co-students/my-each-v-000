37e48b82855cceef7b08233a7141a30363e58460
charlie lee <ctlee9096@gmail.com>
2016-12-18 02:20:48 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
-  # code here
-end
+#require "pry"
+def my_each(collection)
+  i = 0
+  while i < collection.length
+    yield collection[i]
+		i = i + 1
+  end 
+  collection
+end# put argument(s) here
