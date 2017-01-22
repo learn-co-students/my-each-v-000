902a9048c15ca09c2a3b407e396b6365018227eb
Kelsey Huse <kelseyhuse30@gmail.com>
2017-01-22 16:49:59 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,11 @@
-def my_each # put argument(s) here
+def my_each(array) # put argument(s) here
   # code here
+  return_val = []
+  i = 0
+  while i < array.length
+    return_val << array[i]
+    yield array[i]
+    i += 1
+  end
+  return_val
 end
