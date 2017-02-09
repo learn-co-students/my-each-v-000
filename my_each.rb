bd0ca34a31c79d6b1d309be8a1b1fa198dbd27b5
Trai Compton <tlcompton2546@outlook.com>
2017-02-09 21:36:06 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  i = 0
+  new_array = Array.new
+  while i < array.length
+    new_array << array[i]
+    yield array[i]
+    i += 1
+  end
+  new_array
 end
