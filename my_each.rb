7154319373c14cc74e16d33f15f92b029d257cb0
Villy Siu <villysiu@gmail.com>
2017-02-14 23:08:05 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
+def my_each(collection) # put argument(s) here
   # code here
+  index = 0
+  while index < collection.size
+    yield(collection[index])
+    index += 1
+  end
+  collection
 end
