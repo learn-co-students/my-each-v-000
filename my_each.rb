586ef53ad2d7721573cd800e4b569db3868613fa
Sara Hopper <sara.hopper@gmail.com>
2017-03-14 19:01:23 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,13 +1,8 @@
 def my_each(array)
   i = 0
   while i < array.length
-    yield(array[i])
+    yield (array[i])
+    i = i + 1
   end
-    i = i +1
-  end
-end
-
-collection = [1, 2, 3, 4]
-my_each(collection) do |i|
-  puts i
+  array
 end
