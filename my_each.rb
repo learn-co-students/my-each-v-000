71e0c91b87c77d5f12ba4f2f90e38dae3b14e477
Jerred Stahlberg <jerred.stahlberg@yahoo.com>
2017-03-20 15:42:48 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,19 @@
-def my_each # put argument(s) here
+def my_each(array) # put argument(s) here
   # code here
+  i = 0
+  dif_array = []
+  while i < array.length
+    yield array[i]
+    dif_array << array[i]
+  i += 1
+  end
+  dif_array
 end
+
+array = [1, 2, 3, 4]
+my_each(array) do |i|
+   i
+  end
+
+
+
