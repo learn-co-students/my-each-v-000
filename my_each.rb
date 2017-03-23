8ab403bf1191bdf5d494bb4f92f481db289bbe3d
William Barela <william.barela.dev@gmail.com>
2017-03-23 06:15:18 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,6 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array) # put argument(s) here
+  (return "This block should not run!") if array.length < 1
+  i = 0
+  (yield(array[i]); i+= 1) while i < array.length
+  array
 end
