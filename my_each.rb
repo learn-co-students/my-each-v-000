32497a2f7f8cd3f7d799fd3cdd923df7aff22ae2
Harleigh Abel <harleighabel@gmail.com>
2017-03-23 02:12:18 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,18 @@
-def my_each # put argument(s) here
-  # code here
+words = ['hi', 'hello', 'bye', 'goodbye']
+
+def my_each(array)
+  i = 0
+  if array.empty?
+    "This block should not run!"
+  else
+    while i < array.length
+      yield (array[i])
+      i += 1
+    end
+    array
+  end
+end
+
+my_each(words) do |item|
+  item
 end
