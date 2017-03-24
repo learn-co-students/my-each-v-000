e26acf2961c63073eae5a58f85cae451d0a422cb
Jennifer Haddock <jentx123@hotmail.com>
2017-03-24 01:56:17 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -3,9 +3,10 @@ def my_each(array)
   while i < array.size
     yield(array[i])
     i += 1
-  end    
+  end
+  array
 end
 
-my_each[1, 2, 3, 4] do |number|
-  puts number
+my_each([1, 2, 3, 4]) do |number|
+  number
 end
