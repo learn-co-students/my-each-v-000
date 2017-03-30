1622e8f2a6b9f65d3be179c4a33c8e66cf435977
github username <github email address>
2017-03-31 01:44:09 +0900

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+def my_each (argument)
+  i = 0
+  while i<argument.length do
+    yield argument[i]
+    i += 1
+  end
+  argument
+end
+
+my_each(['hi', 'hello', 'bye', 'goodbye']) do |word|
+  print word
 end
