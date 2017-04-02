ec193b5b1083108e9d52c96f30def098db53ee39
David Mars <david.w.mars@my.occc.edu>
2017-04-02 20:25:02 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
-  # code here
+#   ~~~HAVICK WAS HERE~~~
+def my_each(array)
+
+  i=0
+  while i < array.length
+    yield array[i]
+    i+=1
+  end
+nameArray = [['arel', 'jon', 'logan', 'spencer']]
+my_each(nameArray) do
+  |word| return word
+end
 end
