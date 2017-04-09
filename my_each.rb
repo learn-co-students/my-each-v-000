7126bcf3167d80b009289e7dc89dc36c53a0e4c4
Christina Varipapa <christina.varipapa@gmail.com>
2017-04-09 21:18:31 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,7 +1,10 @@
 def my_each(array)
   i = 0
+  #while the index is less than the length of the array
   while i < array.length
+    #yield the element at that index to the block
     yield array[i]
+    #move to the next element
   i = i + 1
   end
   array
