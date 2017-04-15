f0f64040c579745520375713b476e3714e5f17e0
Jacilyn Hayden <jacihayden@gmail.com>
2017-04-15 20:55:31 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,14 @@
-def my_each # put argument(s) here
-  # code here
+words = []
+
+def my_each(words)
+  counter = 0
+  while counter < words.length
+    yield words[counter]
+    counter+= 1
+  end
+  return words
+end
+
+my_each(words) do |i|
+  i = "#{words[counter]}"
 end
