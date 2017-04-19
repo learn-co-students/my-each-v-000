feb76142560a3de7bd8c580c7899b3ce0a7a1a08
Will Sauer <will@heueso.com>
2017-04-19 18:24:35 +0000

    finished

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
-  # code here
+def my_each (array)
+
+  if block_given?
+
+    new_array = array.compact
+    counter = 0
+    while counter < new_array.count
+      yield (new_array[counter])
+      counter += 1
+    end
+  end
+  array
 end
