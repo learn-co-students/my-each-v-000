c1033ed0bdd9c0f1bc7a49def4b0aadedcf0936c
Jeremy Emata <emata@rapidratings.com>
2017-04-24 20:24:53 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array) # put argument(s) here
+  counter = 0
+    while counter < array.length do
+      yield(array[counter])
+      counter += 1
+    end
+    array
 end
+
+collection = [1,2,3,4]
+
+my_each(collection) {|element| element}
