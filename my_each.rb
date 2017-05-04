723633141560c6b6bff96b472f314a8059d3b554
curlywallst <nnoyes@comcast.net>
2017-05-04 15:25:05 -0400

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(collection)
+  iterate = 0
+  while iterate < collection.length
+    yield(collection[iterate])
+    iterate += 1
+  end
+  collection
 end
+
+collection = ["a", "b", "c"]
+
+my_each(collection) {|i| i}
