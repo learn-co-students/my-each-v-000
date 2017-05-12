8b20995aa982e10afefc42cc149384494457fee8
Jenni Macklin <jennimacklin@hotmail.com>
2017-05-12 18:20:01 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
+def my_each (array)
   # code here
+  my_counter = 0
+  while my_counter < array.length
+    yield(array[my_counter])
+    my_counter += 1
+  end
+  array
 end
