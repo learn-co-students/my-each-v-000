8aba7621982ef34d1cceec08b01ace6fab5a64b7
Richard Burd <richardburdx@yahoo.com>
2017-05-15 00:52:42 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array) # put argument(s) here
+  i = 0
+  while i < array.length
+    yield(array[i])
+    i = i + 1
+  end
 end
+# This method should accept an argument of an array and use the while loop to
+# iterate over each member of that array, yielding each element contained in the array to a block.
+
+# in order to get all of the tests passing you'll need to use the yield keyword correctly.
+# Now that you know how the yield method works, try to write your own version of the
+# each method without using the #each method provided by Ruby.
