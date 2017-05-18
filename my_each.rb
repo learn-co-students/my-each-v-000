f18d8427062b17d79dbb59889ff0d99e1cb25c99
Menachem Hornbacher <me@mendel.tech>
2017-05-18 17:38:00 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array) # put argument(s) here
+  # you fucker wanted a while loop here it is
+  while false
+    "False"
+  end
+  for item in array
+    yield item
+  end
+  array
 end
