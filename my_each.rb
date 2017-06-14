d44beeac00efa6b6735f3ca9ad2a72f41bdb3aec
github username <github email address>
2017-06-14 09:47:33 -0400

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
+def my_each(array) # put argument(s) here
   # code here
+  x = 0
+  y = array.count
+  while (x < y)
+      yield array[x]
+      x +=1
+    end
+  array
 end
