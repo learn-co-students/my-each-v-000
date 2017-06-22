3a7035bd30e0066ceaba611998be801f5c07be1b
Eric Strickler <eric.strickler@gmail.com>
2017-06-22 18:22:23 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,11 @@
-def my_each # put argument(s) here
-  # code here
+require'pry'
+
+
+def my_each (array)
+  count = 0
+  while count < array.length
+    yield(array[count])
+    count = count + 1
+  end
+  array
 end
