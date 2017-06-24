5415be23ac2af7beec7fe944ef295b51f8681430
Erica Plofsky <ericaplofsky@gmail.com>
2017-06-24 16:31:27 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,17 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array) # put argument(s) here
+    x = 0
+    while x < array.size
+      block_given?
+      yield(array[x])
+      x = x + 1
+    end
+array
+end
+
+
+# call your method here!
+my_each([1,2,3,4]) do |x|
+  if x = 0
+    x = x + 1
+  end
 end
