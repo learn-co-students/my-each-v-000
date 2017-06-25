25b7f1cf186f9719ebf832c36a45a0e515614cd5
Shelbi Pinkney <shelbi283@gmail.com>
2017-06-25 16:50:58 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
+def my_each(array)
+collection = ["arel", "jon", "logan", "spencer"]    
+  i=0
+  while i < array.length
+  yield array[i]
+  i= i + 1
+end
+collection
+
+   # put argument(s) here
   # code here
 end
