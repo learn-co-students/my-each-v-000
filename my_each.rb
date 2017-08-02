3d8c102dc7909ad84a2c0e7b53c2275786b03b1e
Ariana Berger <ariana.berger@gmail.com>
2017-08-02 15:32:06 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+# array = [1, 2, 3, 4]
+
+def my_each(array)
+    i = 0
+    while i < array.length
+      yield array[i]
+      i += 1
+    end
+  array
 end
+
+# a block is normally needed but it's already provided in the spec
