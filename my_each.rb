07af4822db4c2a47c241f765703d5a43c52e877d
Archana Burra <burra.archana@gmail.com>
2017-08-09 21:50:31 -0400

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,16 @@
-def my_each # put argument(s) here
+def my_each(arr) # put argument(s) here
   # code here
+  if block_given?
+    count = 0
+    while count < arr.length
+      yield arr[count]
+      count+= 1
+    end
+  end
+  arr
+end
+
+collection = [1,2,3,4]
+my_each(collection) do |i|
+  i
 end
