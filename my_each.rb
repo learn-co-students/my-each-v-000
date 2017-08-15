9182383c7cc1cd886d590f285f3c23866d050648
Allen Anderson <dande313@byu.net>
2017-08-15 01:06:50 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,8 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array) # put argument(s) here
+  count=0
+  while count < array.length do
+    yield array[count]
+    count += 1
+  end
+  array
 end
