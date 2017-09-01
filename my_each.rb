21c9d3be2d6ae23995f65848e3a8bb660e855ad7
joey michalina mariano <joey.michalina.mariano@gmail.com>
2017-09-01 06:17:27 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,16 @@
-def my_each # put argument(s) here
+collection = [1,2,3,4]
+
+def my_each(collection) # put argument(s) here
   # code here
+  i = 0
+  while i < collection.count
+    index = collection[i]
+    yield(index)
+    i += 1
+  end
+  collection
+end
+
+my_each(collection) do |index|
+  index
 end
