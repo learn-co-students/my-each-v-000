65fde9c5fcad6034d8b07bf3c00b47728138205b
aisayo <aisayo@icloud.com>
2017-09-05 19:41:11 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array) #takes in array as argument
+    a = 0
+
+  while a < array.length
+    yield array[a]
+    a = a += 1
+  end
+  array
 end
