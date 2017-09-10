a0a5022ff79e04291cf61209b0cdb9e66dd74267
Amber Blahnik <blissedtodays@gmail.com>
2017-09-10 22:35:11 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,21 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  # Set counter to 0
+  counter = 0
+  # While counter is less than array length
+  while counter < array.length
+    # Yield the value of the array at counter
+    yield(array[counter])
+    # Counter incrementing + 1
+    counter += 1
+  end
+  # Return the array
+  array
+end
+
+# Create empty array variable
+array = []
+# Iterate over each index of my_each array
+my_each(array) do |index|
+  # Return each index
+  return index
 end
