ed2faf9d57d9a2c183ade69ec523851592ac68c8
Melissa Rudick <mel.madrugada@gmail.com>
2017-09-19 21:04:34 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(words) # put argument(s) here
+    counter = 0
+    while counter < words.length
+      #words.map do |word|
+
+      yield (words[counter])
+      counter += 1
+    #end
+    # binding.pry
+  end
+  words
 end
