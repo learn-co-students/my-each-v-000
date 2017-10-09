0d4aad8cd35cf747681d242664ce9df11fcb4e23
Alicia Yu <alicia.yu.16@gmail.com>
2017-10-09 21:50:37 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,10 +1,8 @@
 def my_each(words) # put argument(s) here # code here
-  if block_given?
-    index = 0
-    while index < words.length
-      yield(words[index])
-      index += 1
-    end
-    words
+  index = 0
+  while index < words.length
+    yield(words[index])
+    index += 1
   end
+  words
 end
