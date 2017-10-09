6b9a95f0992a2d8b4e519f7f8bf1a35db477a011
Alicia Yu <alicia.yu.16@gmail.com>
2017-10-09 21:48:31 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(words) # put argument(s) here # code here
+  if block_given?
+    index = 0
+    while index < words.length
+      yield(words[index])
+      index += 1
+    end
+    words
+  end
 end
