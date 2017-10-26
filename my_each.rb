4a21c5d0ba50cfb781008ed8a101d92dd33e66c6
Chad Montoya <chadmontoya21@yahoo.com>
2017-10-26 00:00:30 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array) # put argument(s) here
+  if block_given?
+    count = 0
+    while count < array.length
+      yield
+      i += 1
+    end
+  end
 end
