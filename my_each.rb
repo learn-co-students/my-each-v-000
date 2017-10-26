794225dee67425478c86c48ca9af5badf3c88b52
Chad Montoya <chadmontoya21@yahoo.com>
2017-10-26 00:12:24 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,19 @@
+my_array = [1,2,3,4]
+
 def my_each(array) # put argument(s) here
   if block_given?
-    count = 0
-    while count < array.length
-      yield
+    i = 0
+    while i < array.length
+      yield(array[i])
       i += 1
     end
+    array
+  else
+    "Missing Block"
   end
+end
+
+
+my_each(my_array) do |word|
+   word * word
 end
