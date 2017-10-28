d76927b67c875817e87fc43b1373262160abef9e
Elle Faraday <faraday.elle@gmail.com>
2017-10-28 21:06:59 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,15 @@
-def my_each # put argument(s) here
-  # code here
+def my_each (array)
+  i = 0
+if block_given?
+  while i<array.length
+    yield(array[i])
+    i +=1
+  end
+else
+  "Missing a block!"
 end
+  array
+end
+
+array =[1,2,3]
+my_each(array) {|item| item}
