50130fec4761fb7e1c33b59c9e7f82117e14f808
Yina Zhu <yinagit@gmail.com>
2017-11-05 18:40:46 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,22 @@
-def my_each # put argument(s) here
-  # code here
+collection = [1, 2, 3, 4]
+
+def my_each(collection)
+    i = 0
+  while i < collection.length
+  yield collection[i]
+      i += 1
+
+  end
+  collection
 end
+
+# +def my_each(words) # put argument(s) here
+#  +
+#  +  counter = 0
+#  +
+#  +  while counter < words.length
+#  +    yield words[counter]
+#  +    counter = counter + 1
+#  +  end
+#  +  words
+#  +end
