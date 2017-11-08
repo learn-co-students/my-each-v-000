a2e6e83f07a4d1f5d2f9e26e39d95eb28d3c1fab
Ahmad <github email address>
2017-11-07 21:27:10 -0800

    working with yields and blocks

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,18 @@
-def my_each # put argument(s) here
-  # code here
+collection = [1, 2, 3, 4]
+def my_each(collection)
+  if block_given?
+    i = 0
+ 
+    while i < collection.length
+      yield(collection[i])
+      i = i + 1  
+  	end
+  	
+  	collection
+  else
+    "Hey! No block was given!"
+  end
 end
+
+
+my_each(collection) {|i| i}
