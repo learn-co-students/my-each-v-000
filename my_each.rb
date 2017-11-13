cf071cd9f577affabb5c583bd035f49423bd15c0
Ivy Le <ivycle94@gmail.com>
2017-11-13 20:53:14 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(words) # put argument(s) here
+  counter = 0
+
+  while counter < words.size
+    yield words[counter]
+    counter +=  1
+  end
+  words
 end
