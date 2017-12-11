a64cff0ac952a04e5d1cd59afd70960c686d9ad5
Judy McGoldrick <jjeanius@verizon.net>
2017-12-11 17:19:39 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,16 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(word) # put argument(s) here
+  i = 0
+
+  while i < word.length
+    yield(word[i])
+    i = i + 1
+  end
+  word
+  end
+
+my_each (["Hi", "Hello", "Bye", "Goodbye"]) do |word|
+  if word.start_with?("H")
+  "#{word} Stranger!"
+  end
+
 end
