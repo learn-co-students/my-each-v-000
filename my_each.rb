ede042f9e766cd478f4cc084187fc576ca72e276
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2017-12-13 16:56:10 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(arguement)
+  if block_given?
+  i = 0
+while i < arguement.length
+  yield(arguement[i])
+  i += 1
+end
+arguement
+else
+  print "Did you check your block?"
+end
 end
