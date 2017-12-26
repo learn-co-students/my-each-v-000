5b854a224248ea0f39639b29a858acb26938d3c4
EPIC448 <samrey2018@gmail.com>
2017-12-26 05:51:10 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,12 +1,26 @@
-def my_each(array)# put argument(s) here
-    if block_given?
-      i = 0
-      while i < array.length
-        yield array[i]
-        i = i + 1
-      end
-      array
-    else
-    "our collection is different"
- end
+#Both code works great#
+
+
+# def my_each(array)# put argument(s) here
+#     if block_given?
+#       i = 0
+#       while i < array.length
+#         yield array[i]
+#         i = i + 1
+#       end
+#       array
+#     else
+#     "our collection is different"
+#  end
+# end
+
+def my_each(array)
+    i = 0
+
+    while i < array.length
+      yield(array[i])
+      i = i + 1
+    end
+
+    array
 end
