98bf780ccc74607f55ae7b24ca0935aca746626c
Petya Djanovska <petya.djanovska@gmail.com>
2018-01-02 21:23:35 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
+def my_each(array)# put argument(s) here
   # code here
+  index = 0
+  while index < array.length
+    yield array[index]
+    index = index + 1
+  end
+  array
 end
