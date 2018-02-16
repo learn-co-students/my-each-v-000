f3ab6e17f479c438a8bcd99168b33d23c1c846c9
emi kojima <emi8emi@gmail.com>
2018-02-16 20:50:34 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,11 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  count = 0
+    while array.count != count
+    yield array[count]
+  count += 1
+    end  
+  array
 end
+  
+
+ 
