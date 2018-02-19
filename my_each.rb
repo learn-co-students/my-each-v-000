211cd5335f9df9999f347a74a25e663effe321d2
Allyson Hotchkin <ahotchk1@yahoo.com>
2018-02-19 01:59:12 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,6 +1,12 @@
 def my_each(array)
-  i = 0
-  while i < array.length
-    yield(array[i])
+  if block_given?
+    i = 0
+    while i < array.length
+      yield(array[i])
+      i += 1
+    end
+    array
+  else
+    "This block should not run!"
   end
 end
