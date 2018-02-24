b572464a68972a01152c04f1efc961d17c3239c7
Lauren Pardee <lap5151@gmail.com>
2018-02-24 20:32:51 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  i = 0
+   while i < array.length
+    yield array[i]
+    i = i + 1
+  end
+  array
+end
+
+my_each(["Lauren", "Jasper", "Prasanna"]) do |x|
+  x
 end
