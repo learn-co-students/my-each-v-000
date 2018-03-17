915863e2a1a807927418f2eed7d9ffc5a4a5ddb2
Kitty Davies <kittyfdavies@gmail.com>
2018-03-17 21:15:02 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
+def my_each(array) # put argument(s) here
   # code here
+  index = 0
+  while array[index]
+    yield array[index]
+    index += 1
+  end
+  array
 end
