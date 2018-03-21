20db408073e9fea27daa1ad40fd514122c59f647
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-03-21 22:39:27 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,11 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(collection)
+  i = 0
+  while i < collection.length #works with arrays too!
+   yield collection[i]
+   #yield is used without inserting anything. 
+   #no reason to and can't because inserting a line ({} or do)
+   #doesn't work without the access of the variables that are defined in the method.
+    i += 1
+  end
+  collection
 end
