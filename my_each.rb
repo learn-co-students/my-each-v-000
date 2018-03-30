fcf5f1e41d2e4117a074d5b7247f9bf8a2b6c1fd
Chris Dundon <chris.dundon@gmail.com>
2018-03-30 20:22:24 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,14 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(arg)
+  if !block_given?
+    return "No block supplied!"
+  end
+  
+  mIdx = arg.length - 1
+  idx = 0
+  while (idx <= mIdx) do
+    
+    yield arg[idx]
+    idx += 1
+  end
+  arg
 end
