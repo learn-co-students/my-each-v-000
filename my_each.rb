c82f66befec65eb6679d986669a477729c8c947c
Joshua Tan <jtsarvida@gmail.com>
2018-05-02 14:45:59 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  i=0 
+  while i < array.length
+    item = array[i]
+    yield(item)
+    i += 1 
+  end
+  return array
 end
+
