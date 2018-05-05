4177e975e25c2e4e831b7e658535be970f24ddde
Carrie Savinell <carrielane89@gmail.com>
2018-05-05 13:56:33 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,14 @@
-def my_each # put argument(s) here
-  # code here
+collection_array = ['hi', 'hello', 'bye', 'goodbye']
+
+def my_each(collection_array) 
+  i = 0 
+  while i < collection_array.length 
+    yield collection_array[i]
+    i = i + 1
+  end 
+  collection_array
 end
+
+my_each(collection_array) do |i|
+   i 
+end   
