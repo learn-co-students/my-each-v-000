d911a56cfa84e629c7d39464718661bf23641938
Carrie Savinell <carrielane89@gmail.com>
2018-05-11 20:46:56 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,14 +1,14 @@
 collection_array = ['hi', 'hello', 'bye', 'goodbye']
 
-def my_each(collection_array) 
+def my_each(collection_array)
   i = 0 
   while i < collection_array.length 
     yield collection_array[i]
-    i = i + 1
+    i = i + 1 
   end 
-  collection_array
-end
+  collection_array 
+end 
 
 my_each(collection_array) do |i|
-   i 
+  i 
 end   
