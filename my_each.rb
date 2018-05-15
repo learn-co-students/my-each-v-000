16edcadb2f0fd29f79ac10b59ed785385ab5e996
Brennan Fulmer <brennan.flmr@gmail.com>
2018-05-15 18:52:36 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(proc = nil)
+  length.times do |index|
+    if proc == nil
+      yield(self[index])
+    else
+      proc.call(self[index])
+    end
+  end
+  self
 end
