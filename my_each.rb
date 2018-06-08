345a642c8c4705f28e6039d0f2177a9f86ecd683
Courtney Haltiwanger <chalt0319@gmail.com>
2018-06-08 18:33:39 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,14 @@
-def my_each # put argument(s) here
-  # code here
+words = ['hi', 'hello', 'bye', 'goodbye']
+
+def my_each(array)
+  counter = 0
+  while counter < array.length 
+    yield array[counter] if block_given?
+    counter += 1 
+  end
+  array
 end
+
+my_each(words) do |i|
+  i 
+end 
