e38b9f3ca95df99199b7c51a105ec72349e5128a
Gabriel Valle <gvalle91@gmail.com>
2018-06-14 00:36:34 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,11 @@
-def my_each # put argument(s) here
+def my_each(collection)
   # code here
+  counter = 0
+  new_array = []
+  while counter < collection.length
+    yield collection[counter]
+    new_array << collection[counter]
+    counter += 1
+  end
+  return collection
 end
