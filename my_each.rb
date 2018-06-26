aab314e60a1ed9e2110243d2f685b0e22541a488
Jordan Harris <jharris@wework.com>
2018-06-26 19:22:05 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,12 +1,3 @@
-# def my_each(collection) do |i|
-#     times_called = 0
-#     while collection.any? != false
-#       times_called += 1
-#       # collection |i| do 
-#     yield i
-#   end
-# end
-
 def my_each(array)
     if block_given?
       i = 0
