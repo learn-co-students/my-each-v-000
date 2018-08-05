aaf1561f17d5352c314be13d55fadea31c434650
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-08-05 18:04:35 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,20 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(arr)
+  
+  word = 0 
+  while arr.length >= 0 
+    word += 1
+    yield
+    puts word
+  end
+
 end
+
+
+# tests
+
+
+# must iterate over every element with while
+# must use yield so that we return 
+# an array with same elements as original
+
+# run block n times (iterating)
