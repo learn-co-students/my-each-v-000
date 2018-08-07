7ab13d77165caeaf7c1b892c6bd0abe95bf3f83f
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-08-07 01:44:08 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,20 +1,12 @@
+
+
 def my_each(arr)
   
-  word = 0 
-  while arr.length >= 0 
-    word += 1
-    yield
-    puts word
+  i = 0 
+  while i < arr.length
+  yield(arr[i])
+    i += 1 
   end
-
+  arr
 end
 
-
-# tests
-
-
-# must iterate over every element with while
-# must use yield so that we return 
-# an array with same elements as original
-
-# run block n times (iterating)
