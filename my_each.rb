2290994d566883f58fe97e2d2738155e25be058d
Steven Crouse <sdcrouse91@gmail.com>
2018-08-11 06:57:03 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,15 @@
-def my_each # put argument(s) here
+def my_each(array) # put argument(s) here
   # code here
+  
+  if block_given?
+    counter = 0 
+    while counter < array.size 
+      yield array[counter]
+      counter += 1
+    end
+    
+    array
+  else
+    "You need to pass a block!"
+  end
 end
