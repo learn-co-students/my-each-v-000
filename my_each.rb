b966e0f8527e9b33ec796ea4e59a6bb64d56159f
Karly Mareka <karly.mareka@gmail.com>
2018-08-29 22:32:06 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,14 @@
-def my_each # put argument(s) here
-  # code here
+array_numbers = [1,2,3,4]
+
+def my_each(array_numbers)
+  i = 0 
+  while i < array_numbers.length
+    yield(array_numbers[i])
+    i += 1 
+  end
+  array_numbers
 end
+
+my_each(array_numbers) do |word| 
+ print word 
+end 
