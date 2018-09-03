f40fa7ed5e0a17e17990c4a40ec3eda311de711c
alice T <alicewtien@gmail.com>
2018-09-03 23:19:46 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,8 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(collection) # put argument(s) here
+  i = 0
+  while i < collection.length #i(index) is < length of array or it loop forever
+    yield(collection[i])
+    i += 1
+  end
+  collection #calling original array so the return value is not nil
 end
