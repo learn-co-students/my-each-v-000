bd151f650ede9ce4624a99273f511cfd1e9f713d
Amil Masic <amil.masic@gmail.com>
2018-09-12 02:49:57 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,15 @@
-def my_each # put argument(s) here
+def my_each(collection) # put argument(s) here
   # code here
+  # if block_given?
+    i = 0
+    while i < collection.length
+      yield collection[i]
+      i +=1
+    # else
+
+  end
+  collection
+end
+my_each([]) do |word|
+   word
 end
