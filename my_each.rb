59d6af77f854edef8eb9312e070627da26093e57
John Blank <veloblank@gmail.com>
2018-09-12 09:33:26 -0400

    Refactor.

--- my_each.rb
+++ my_each.rb
@@ -2,7 +2,8 @@ def my_each(array)
   i = 0
   while i < array.length
     yield array[i]
-    i = i + 1
+    i += 1 #change to increment and assign
   end
-  return array
+  #no need for explicit 'return'
+  array
 end
