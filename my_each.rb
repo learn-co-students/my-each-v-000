e5c39468d1a2652250ff9529c73ebc6b01ab0a6a
Eva Navarro <emanao@me.com>
2019-10-31 18:29:09 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,12 @@
 def my_each (array)# put argument(s) here
-  # code here
-  counter=0
-  while counter<array.length
-    yield array[counter]
-    counter += 1
+  if block_given?
+    counter=0
+    while counter<array.length
+      yield array[counter]
+      counter += 1
+    end
+    array
+  else
+    "Hey! No block was given!"
   end
-  array
 end
