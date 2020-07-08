4c578717078ae716fafab297807607118ae8e138
hkato4188 <hkato@sas.upenn.edu>
2020-07-08 14:03:10 -0400

    defined method to take a block optionally

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,12 @@
 def my_each(array)
   i = 0
-    while i < array.length
+  while i < array.length
+    if block_given?
       yield(array[i])
-        i = i + 1
-      end
-    array
+    end
+    i +=1
+  end
+  array
 end
+
+# call your method here!
